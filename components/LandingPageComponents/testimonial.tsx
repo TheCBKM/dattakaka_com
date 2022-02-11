ed799417c63916f449e66012814cb464f6a04e38
Usman Haider <haideru448@gmail.com>
2022-02-11 14:43:15 +0500

    changed responsiveness

--- components/LandingPageComponents/testimonial.tsx
+++ components/LandingPageComponents/testimonial.tsx
@@ -29,12 +29,12 @@ function TestimonialsPage() {
 
             /></Item>
           </Grid>
-          <Grid item xs={8} md={4} >
+          <Grid item xs={8} md={4} className={styles.testimonialPaddingClass}>
             <Item className={styles.GridPadding}><Typography variant="h3">About</Typography><br></br>
-              <Typography variant="h4">The Brief</Typography>
+              <Typography variant="h5">The Brief</Typography>
               <br></br>
               <br></br>
-              <Typography sx={{ padding: 5 }}>Being Happy is the most vital, valuable thing in life. If one is not happy means he doesnt have the right thought process. When it comes to Saints, they are the happiest soul alive. They have understood life, accepted the concepts and now making use of them to live a happy life. So to be happy; the easiest way is to align with Saints thought process by listening and accepting him. A modern and recent Saint who used latest computer technology in a professional organization, LIC, the disciple of Gondavlekar Maharaj, Namyogi Datta Kaka ( D P Kulkarni) has many books on guidance on happy life at the home, office, business..in a group or alone..young, middle-age or riped..( Sukh samvad).</Typography><br></br><br></br>
+              <Typography sx={{ padding: 1.5 }}>Being Happy is the most vital, valuable thing in life. If one is not happy means he doesnt have the right thought process. When it comes to Saints, they are the happiest soul alive. They have understood life, accepted the concepts and now making use of them to live a happy life. So to be happy; the easiest way is to align with Saints thought process by listening and accepting him. A modern and recent Saint who used latest computer technology in a professional organization, LIC, the disciple of Gondavlekar Maharaj, Namyogi Datta Kaka ( D P Kulkarni) has many books on guidance on happy life at the home, office, business..in a group or alone..young, middle-age or riped..( Sukh samvad).</Typography><br></br><br></br>
 
               <Button variant="contained" sx={{ background: "black" }}>Must Read</Button>
             </Item>
